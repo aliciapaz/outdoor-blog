213eed7e94dbd68f2c22de6c9051a3519b1fd214
aliciapaz <aliciapazrojas@gmail.com>
2021-03-22 18:04:34 -0300

    log in page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # GET /users/1/edit
-  def edit
-  end
-
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
@@ -26,33 +22,9 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /users/1 or /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1 or /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
@@ -64,6 +36,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:name)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :articles
-  resources :users
+  resources :users, only: %i[new create show]
+  resources :categories, only: %i[new create show]
+  resources :articles, only: %i[index new create show] do
+    resources :votes, only: %i[new create destroy]
+  end
+
+  root 'articles#index'
+
+  # logout
+  get    '/login',   to: 'users#new'
+  post   '/login',   to: 'users#create'
+  post '/logout',  to: 'users#logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
