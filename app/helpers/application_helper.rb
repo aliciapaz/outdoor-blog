3641c384e6d6df17c18a9b1a0fc89f55de7649a9
aliciapaz <aliciapazrojas@gmail.com>
2021-03-23 17:17:43 -0300

    add navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,18 @@
 module ApplicationHelper
   def navbar_button_login
-    link_to 'LOGIN', '/login' unless user_signed_in?
+    link_to 'LOGIN', '/login', class: 'navbar-link' unless user_signed_in?
   end
 
   def navbar_button_sign_up
-    link_to 'REGISTER', signup_path unless user_signed_in?
+    link_to 'REGISTER', signup_path, class: 'navbar-link' unless user_signed_in?
   end
 
   def navbar_button_logout
-    button_to 'LOGOUT', logout_path, { method: :delete, data: { confirm: 'Are you sure?' } } if user_signed_in?
+    button_to 'LOGOUT', logout_path, { method: :delete, data: { confirm: 'Are you sure?' } }, class: 'navbar-link' if user_signed_in?
   end
 
   def navbar_button_name
-    link_to current_user.name, user_path(current_user) if user_signed_in?
+    link_to current_user.name, user_path(current_user), class: 'navbar-link' if user_signed_in?
   end
 
   private
