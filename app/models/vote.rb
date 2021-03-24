4f849dedf0a7507aac268f7354d905e439fecc7b
aliciapaz <aliciapazrojas@gmail.com>
2021-03-24 14:56:31 -0300

    add test unit for most voted article

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,13 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :article
 
-  def self.vote_count
+  def self.count_by_article
     self.group(:article_id).order('count_all desc').count
+  end
+
+  def self.count_by_category
+    joins('INNER JOIN articles ON articles.id = votes.article_id')
+      .joins('INNER JOIN categories ON categories.id=articles.category_id')
+      .group(:category_id).order('count_all desc').count
   end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    
+  end
+  it 'retrieves the count of votes grouped by article' do
+    expect(self.count_by_article).to eq()
+  end
+
+  it 'retrieves the cout of votes grouped by category' do
+  
+  end
 end
