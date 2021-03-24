343453bb77090a41beb4dfaebf7dc6f20c8159f6
aliciapaz <aliciapazrojas@gmail.com>
2021-03-24 15:30:43 -0300

    add tests for vote count

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,13 +2,36 @@ require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
   before(:all) do
-    
-  end
-  it 'retrieves the count of votes grouped by article' do
-    expect(self.count_by_article).to eq()
+    4.times do
+      create(:user)
+    end
+
+    4.times do
+      create(:category)
+    end
+
+    2.times do
+      create(:article)
+    end
+
+    10.times do
+      create(:vote, article_id: '1')
+    end
+
+    5.times do
+      create(:vote, article_id: '2')
+    end
   end
 
-  it 'retrieves the cout of votes grouped by category' do
-  
+  it 'retrieves the count of votes grouped by article sorted in descending order' do
+    expect(Vote.count_by_article.class).to eq(Hash)
+    expect(Vote.count_by_article.keys[0]).to eq(1)
+    expect(Vote.count_by_article.values[0]).to be > Vote.count_by_article.values[1]
+  end
+
+  it 'retrieves the count of votes grouped by category' do
+    expect(Vote.count_by_category.class).to eq(Hash)
+    expect(Vote.count_by_category.keys[0].class).to eq(Integer)
+    expect(Vote.count_by_category.values[0]).to be > Vote.count_by_category.values[1]
   end
 end
