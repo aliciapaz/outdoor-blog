48e4e8cf7883a135a532ac63bce0a8bcd12f9bcf
aliciapaz <aliciapazrojas@gmail.com>
2021-03-25 18:16:58 -0300

    add button to vote

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -6,6 +6,7 @@ module CategoriesHelper
       concat content_tag(:h2, article.title, class: 'article-snippet-title')
       concat content_tag(:p, article.text.truncate_words(15, omission: '...'), class: 'article-snippet-text')
       concat content_tag(:p, "By  #{User.find(article.author_id).name.titleize}", class: 'article-snippet-author')
+      concat button_to 'Vote!', article_votes_path(article[:id]), params: { vote: {user_id: current_user.id, article_id: article.id } } if user_signed_in?
     end
   end
 end
