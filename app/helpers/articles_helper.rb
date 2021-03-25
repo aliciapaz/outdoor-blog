b62ea496b42e2e999d68d8878ca6859365ec95b6
aliciapaz <aliciapazrojas@gmail.com>
2021-03-25 02:09:27 -0300

    small fixes in index and navbar

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
   def category_article_item(article)
-    concat link_to Category.find(article.category_id).name, article, class: 'category-title'
+    concat link_to Category.find(article.category_id).name.titleize, article, class: 'category-title'
     concat image_tag article.image, class: 'category-article-image'
     link_to article.title, article, class: 'category-article-title'
   end
