a7ab5594ec303914e715e63f4782c23f60ba9ad0
aliciapaz <aliciapazrojas@gmail.com>
2021-03-26 17:19:44 -0300

    test registration view

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'users/new', type: :view do
   before(:each) do
-    @user = assign(:user, User.create!(
-                                  name: 'My Name',
-                                ))
+    assign(:user, User.new(
+                      name: 'MyString',
+                     ))
   end
 
-  it 'renders attributes in form' do
+  it 'renders new user form' do
     render
-    expect(rendered).to match(/My Name/)
+    assert_select 'input[name=?]', 'user[name]'
   end
 end
