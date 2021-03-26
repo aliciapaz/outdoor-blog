a51766553b6c27881fbe2f89278950c9d7ef87e9
aliciapaz <aliciapazrojas@gmail.com>
2021-03-26 17:17:09 -0300

    add tests for view

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'users/new', type: :view do
+  before(:each) do
+    @user = assign(:user, User.create!(
+                                  name: 'My Name',
+                                ))
+  end
+
+  it 'renders attributes in form' do
+    render
+    expect(rendered).to match(/My Name/)
+  end
 end
