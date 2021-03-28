7f406886a38f2a6c59ef484d5d6d0fd206359b47
aliciapaz <aliciapazrojas@gmail.com>
2021-03-28 13:18:22 -0300

    remove logic from view in  article#show

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -5,4 +5,14 @@ module ArticlesHelper
     concat image_tag article.image, class: 'category-article-image'
     link_to article.title, article, class: 'category-article-title'
   end
+
+  def author_name(article)
+    User.find(article.author_id).name.titleize
+  end
+
+  def category_link(article)
+    link_to Category.find(article.category_id).name.titleize,
+                   category_path(Category.find(article.category_id)),
+                   class: 'category-link'
+  end
 end
