510639011e68b53ad25f94d405abec5cc5488739
aliciapaz <aliciapazrojas@gmail.com>
2021-03-28 12:28:09 -0300

    empty develop branch

--- app/helpers/articles_helper.rb
+++
@@ -1,8 +0,0 @@
-module ArticlesHelper
-  def article_item(article)
-    concat link_to Category.find(article.category_id).name.titleize,
-                   category_path(Category.find(article.category_id)), class: 'category-title'
-    concat image_tag article.image, class: 'category-article-image'
-    link_to article.title, article, class: 'category-article-title'
-  end
-end
--- spec/helpers/articles_helper_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ArticlesHelper, type: :helper do
-end
